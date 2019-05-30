5c36b131394ea9e9b60f8abed75c7aa7f58b196f
rodrimozocubaz1 <rodrigo.mozo.01@gmail.com>
2019-05-30 15:58:59 -0500

    Contac

---
+++ contactar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contactacnos</title>
+</head>
+<body>
+    <h1>Telefono</h1>
+    <p>957632841</p>
+    <h2>Direccion</h2>
+    <p>Googlemaps</p>
+    <h3>Facebook</h3>
+    <p><a href="https://es-la.facebook.com/"></a></p>
+</body>
+</html>
---
+++ quienes_somos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>¿Qué es TuPet?</h1>
+    <p>TuPet es una asociación sin fines de lucro dedicada a la promoción de la adopción, el trabajo de concientización sobre la realidad de los perros en estado de abandono y la ejecución de proyectos que contribuyan a la construcción de un mundo mejor para todos los perros.</p>
+    <h2>Mision</h2>
+    <p>Conectamos a la comunidad con la realidad de los perros en abandono, a través de un sistema auto sostenible con soluciones que contribuyen a la construcción de un mundo mejor para todos los perros.</p>
+    <h3>Vision</h3>
+    <p>Construir un mundo donde todos los perros sean felices.</p>
+</body>
+</html>
