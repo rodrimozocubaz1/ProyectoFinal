0fabb7b0766f031f1598d4bb7b446a375864e784
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-30 15:50:39 -0500

    actualizar variables

--- variables.php
+++ variables.php
@@ -1,23 +1,23 @@
 <?php
 $tab_usuario="usuario";
-$id_usuario="id_usuario";
-$user_usuario="nom_usuario";
-$nombres_usuario="nombres";
-$apellidos_usuario="apellidos";
+$id_usuario="id";
+$user_usuario="usuario";
+$nom_usuario="nombres";
+$ape_usuario="apellidos";
 $fecha_nac_usuario="fecha_nac";
 $pass_usuario="password";
 
 $tab_mascota="mascotas";
-$id_mascota="id_mascota";
-$nom_mascota="nom_mascota";
+$id_mascota="id";
+$nom_mascota="nombre";
 $due_mascota="dueño";
 $raza_mascota="raza";
 $color_mascota="color";
-$tamanio="tamaño";
+$tam_mascota="tamaño";
 
 $tab_vacunas="vacunas";
-$id_vacunas="id_vacunas";
-$nom_vacunas="nom_vacunas"
+$id_vacunas="id";
+$nom_vacunas="nombre"
 $descripcion="descripcion"
 
 
