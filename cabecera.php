57a36e8dec2b017f4ec58e09b62befede51b894e
RenattoB <renattobaca@gmail.com>
2019-05-30 17:42:32 -0500

    Añadiendo flex

--- cabecera.php
+++ cabecera.php
@@ -1,21 +1,21 @@
 <header>
         <div class="navegacion">
-            <div >
-                <nav class="navegacion1_div">
+            <div class="navegacion1_div">
+                <nav>
                     <ol>
-                        <li class="lista"><a href="adopcion.php">Adopcion</a></li>
-                        <li class="lista"><a href="Talleres.php">Talleres</a></li>
-                        <li class="lista"><a href="Contactanos.php">Contactanos</a></A></li>   
-                        <li class="lista"><a href="iniciar.php">Iniciar Sesión</a></li>
-                        <li class="lista"><a href="registrar.php">Registrarse</a></li>        
+                        <li class="lista"><a href="adopcion.php" class="a">Adopcion</a></li>
+                        <li class="lista"><a href="Talleres.php" class="a">Talleres</a></li>
+                        <li class="lista"><a href="Contactanos.php" class="a">Contactanos</a></A></li>   
+                        <li class="lista"><a href="iniciar.php" class="a">Iniciar Sesión</a></li>
+                        <li class="lista"><a href="registrar.php" class="a">Registrarse</a></li>        
                     </ol>    
                 </nav>
             </div>
             <div class="navegacion2_div">
                 <nav>
                     <ol>
-                        <li><a href="login.php">Iniciar Sesion</a></li>
-                        <li><a href="registar.php">Registrarse</a></li>
+                        <li class="lista"><a href="login.php" class="a">Iniciar Sesion</a></li>
+                        <li class="lista"><a href="registar.php" class="a">Registrarse</a></li>
                     </ol>
                 </nav>
             </div>
