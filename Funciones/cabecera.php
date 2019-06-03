7144a0c4b0becd9b7de664656437d5d09c8fee17
RenattoB <renattobaca@gmail.com>
2019-06-02 21:47:30 -0500

    Terminando el header principal

--- Funciones/cabecera.php
+++ Funciones/cabecera.php
@@ -1,10 +1,10 @@
 <header class="site-header inicio">    
-    <div class='contenedor'>
+    <div class='contenedor contenido-header'>
         <div class="barra">
-            <a href="/"><img src="Imagenes/Logo.png" alt="Logo" width="200px"></a>
+            <a href="index.php"><img src="Imagenes/Logo.png" alt="Logo" width="200px"></a>
             <nav class="navegacion">                    
-                <a href="adopcion.php" class="a">Adopcion</a>
-                <a href="Talleres.php" class="a">Talleres</a>
+                <a href="paginas/adopcion.php" class="a">Adopcion</a>
+                <a href="paginas/talleres.php" class="a">Talleres</a>
                 <a href="paginas/contactar.php" class="a">Contactanos</a>
                 <a href="paginas/quienes_somos.php" class="a">Nosotros</a>               
                 <a href="Funciones/login.php" class="a">Iniciar Sesion</a>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>TuPET</title>
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="estilos/estilo.css">
 </head>
 <body>
@@ -14,10 +15,6 @@
 
     <main>
         
-
-
-
-
     </main>
     
 
