597d4a132661e21d70d27036e08cb09599673326
Marielena.Dominguez <marielena.dominguez@usil.pe>
2019-06-05 12:26:43 -0500

    correcciones menores del procesar_login

--- Funciones/procesar_login.php
+++ Funciones/procesar_login.php
@@ -22,18 +22,18 @@ WHERE '$user_usuario' = '$usuario'";
 
 #ejecutar comando
 $resultado=$pdo->query($sql);
-$filas=$resultado->fetchAll();
+$fila=$resultado->fetchAll();
 
 #verifica si hay un usuario con el nombre de usuario proporcionado
-if(count($filas)==1){
-    if($fila[$pass_usuario]==$password){
+if(count($fila)==1){
+    if($fila[$pass_usuario]==md5($password)){
         #login satisfactorio
         #se inicia la sesion y se guardan las variables usuario, nombres, apellidos y email
         session_start();
-        $_SESSION["usuario"]=$filas[$user_usuario];
-        $_SESSION["nombres"]=$filas[$nom_usuario];
-        $_SESSION["apellidos"]=$filas[$ape_usuario];
-        $_SESSION["email"]=$filas[$email_usuario];
+        $_SESSION["usuario"]=$fila[$user_usuario];
+        $_SESSION["nombres"]=$fila[$nom_usuario];
+        $_SESSION["apellidos"]=$fila[$ape_usuario];
+        $_SESSION["email"]=$fila[$email_usuario];
 
         #entra si desea crear la cookie
         if(isset($_POST["s"]) && $_POST["s"]=="1"){
