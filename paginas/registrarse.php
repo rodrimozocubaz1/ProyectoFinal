ef9fec848ade5d42c8ebdd46c17ef4fe96264769
RenattoB <renattobaca@gmail.com>
2019-06-13 14:38:11 -0500

    Mejorando el diseño de la pagina

--- paginas/registrarse.php
+++ paginas/registrarse.php
@@ -8,8 +8,10 @@ include ("../Funciones/variables.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Registrarse</title>
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../estilos/estilonosotros.css">
     <link rel="stylesheet" href="../estilos/registrarse.css">
+    
 </head>
 
 <body>
