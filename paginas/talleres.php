8cbe880102d02e40840332b7ddba6aee7ca80921
RenattoB <renattobaca@gmail.com>
2019-06-16 18:47:52 -0500

    pequeñas modificaciones

--- paginas/talleres.php
+++ paginas/talleres.php
@@ -15,6 +15,7 @@ $sql="SELECT * FROM $tab_talleres ORDER BY $id_taller ASC";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Talleres</title>
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../estilos/estilonosotros.css">    
 </head>
 <body>
