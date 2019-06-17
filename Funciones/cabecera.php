6eb694596ee7fdb99a0f2c3544094b251f1ceb69
Gianfranco Mello <gianko85@gmail.com>
2019-06-16 23:00:21 -0500

    cambios cabeceras

--- Funciones/cabecera.php
+++ Funciones/cabecera.php
@@ -3,8 +3,10 @@
         <div class="barra">
             <a href="index.php"><img src="Imagenes/Logo.png" alt="Logo" width="200px"></a>
             <nav class="navegacion">                    
+                <a href="paginas/reportar_mascota.php" class="a">Reportar</a>
                 <a href="paginas/adopcion.php" class="a">Adopcion</a>
                 <a href="paginas/talleres.php" class="a">Talleres</a>
+                <a href="paginas/vacunacion.php" class="a">Vacunación</a>
                 <a href="paginas/contactar.php" class="a">Contactanos</a>
                 <a href="paginas/quienes_somos.php" class="a">Nosotros</a>               
                 <?php if(isset($_SESSION["id"])){ ?>
