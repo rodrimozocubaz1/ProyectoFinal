2461d2bd10af9a7bfca0fe0b2f5728874b2dc3c9
RenattoB <renattobaca@gmail.com>
2019-06-17 10:16:05 -0500

    Cambio a los botones

--- paginas/contactar.php
+++ paginas/contactar.php
@@ -10,8 +10,7 @@ include ("../Funciones/verificar_session.php");
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contáctanos</title>
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../estilos/estilonosotros.css">
-    <link rel="stylesheet" href="estilos/estiloprincipal.css">
+    <link rel="stylesheet" href="../estilos/estilonosotros.css">    
 </head>
 <body>
     <?php include ('../Funciones/cabecera_resto.php') ?>
@@ -21,7 +20,7 @@ include ("../Funciones/verificar_session.php");
             <p>957632841</p>
             <h2>Direccion</h2>
             <p>
-                <a href="mailto:tupetcito@gmail.com">Click aqui para escribirnos un correo</a>
+                <a href="mailto:tupetcito@gmail.com" class="enlaces">Click aqui para escribirnos un correo</a>
             </p>
             <div class="contenedor_maps">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31207.181861621746!2d-77.04260745654548!3d-12.11914934144648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c83a81adca93%3A0xd8b7788114b65646!2sVeterinaria+Miraflores!5e0!3m2!1ses!2spe!4v1560745171943!5m2!1ses!2spe" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -30,7 +29,7 @@ include ("../Funciones/verificar_session.php");
             <h3>Facebook</h3>
         
             <p>
-                <a href="https://www.facebook.com/paco.tupet.5">TuPet FB ( clickea )</a>
+                <a href="https://www.facebook.com/paco.tupet.5" class="enlaces">TuPet FB ( clickea )</a>
             </p>
         <main>
     </div>    
