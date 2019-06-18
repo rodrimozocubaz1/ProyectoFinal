d660503e4b9877dc010b3a0632695d38edab68b6
RenattoB <renattobaca@gmail.com>
2019-06-18 12:59:42 -0500

    añadiendo imagenes

--- paginas/contactar.php
+++ paginas/contactar.php
@@ -14,8 +14,7 @@ include ("../Funciones/verificar_session.php");
 </head>
 <body>
     <?php include ('../Funciones/cabecera_resto.php') ?>
-    <div class="contenido_contactar">
-        <main>
+    <div class="contenido_contactar">        
             <h1>Telefono</h1>
             <p>957632841</p>
             <h2>Direccion</h2>
@@ -24,14 +23,12 @@ include ("../Funciones/verificar_session.php");
             </p>
             <div class="contenedor_maps">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31207.181861621746!2d-77.04260745654548!3d-12.11914934144648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c83a81adca93%3A0xd8b7788114b65646!2sVeterinaria+Miraflores!5e0!3m2!1ses!2spe!4v1560745171943!5m2!1ses!2spe" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-            
+            </div>            
             <h3>Facebook</h3>
+            <div class="boton_face">
+                <a href="https://www.facebook.com/paco.tupet.5" class="enlaces">TuPet FB ( clickea )</a>           
+            </div>
         
-            <p>
-                <a href="https://www.facebook.com/paco.tupet.5" class="enlaces">TuPet FB ( clickea )</a>
-            </p>
-        <main>
     </div>    
     <?php include ('../Funciones/footer_resto.php') ?>
 </body>
