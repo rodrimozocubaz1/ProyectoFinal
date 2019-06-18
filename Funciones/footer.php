93a39ce0f2fe9da35bd67217da3b39f576dd958e
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-18 14:14:18 -0500

    arreglo links footers

--- Funciones/footer.php
+++ Funciones/footer.php
@@ -1,12 +1,12 @@
     <footer>  
         <div class="contenedor contenedor-footer"> 
-            <nav class="navegacion nav_footer">                    
+            <nav class="navegacion nav_footer">
+                <a href="paginas/reportar_mascota.php" class="a">Reportar</a>
                 <a href="paginas/adopcion.php" class="a">Adopcion</a>
                 <a href="paginas/talleres.php" class="a">Talleres</a>
+                <a href="paginas/vacunacion.php" class="a">Vacunación</a>                
                 <a href="paginas/contactar.php" class="a">Contactanos</a>
-                <a href="paginas/quienes_somos.php" class="a">Nosotros</a>               
-                <a href="Funciones/login.php" class="a">Iniciar Sesion</a>
-                <a href="Funciones/registarse.php" class="a">Registrarse</a>                    
+                <a href="paginas/quienes_somos.php" class="a">Nosotros</a>                             
             </nav> 
         </div>     
     </footer>   
